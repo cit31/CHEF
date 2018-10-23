3744ca50f6f9f71b30f2a365c2842b00acbed5f5
Raghu K <raghuk.vit@gmail.com>
2018-10-23 02:01:49 +0000

    Updating CHEF

---
+++ install-chef-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum install html2text -y &>/dev/null
+URL=$(curl https://downloads.chef.io/chef-server/12.18.14 | html2text | grep el7.x86_64.rpm$ | awk '{print $NF}')
+sudo yum install $URL -y 
+sudo chef-server-ctl reconfigure
+sudo chef-server-ctl user-create admin Admin User admin@local.com 'PASSWORD' --filename /opt/admin.pem 
+sudo chef-server-ctl org-create student 'Student App' --association_user admin --filename /opt/student.pem 
+sudo chef-server-ctl install chef-manage
+sudo chef-server-ctl reconfigure
+sudo chef-manage-ctl reconfigure
